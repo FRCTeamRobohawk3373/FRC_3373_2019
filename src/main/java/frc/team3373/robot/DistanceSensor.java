31866edd2f1e889e7da7231507835e874a4ceea3
Robohawks <RoboHawks@10.0.28.28>
2019-10-05 11:06:12 -0400

    Made changes for testing

--- src/main/java/frc/team3373/robot/DistanceSensor.java
+++ src/main/java/frc/team3373/robot/DistanceSensor.java
@@ -6,41 +6,16 @@ import org.json.JSONException;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class DistanceSensor {
-
-    double[][] table;
-
     private AnalogInput sensor; // Analog sensor
-
-    private boolean useLookup = false;
 
     private double a;
     private double b;
     private double c;
-    /* private double d;
-    private double e;
-    private double f; */
 
     private double pDist;
     private long lastUpdateMicros;
 
     Constants con = new Constants();
-
-    /**
-     * Class for instantiating a distance sensor using a lookup table (Unused)
-     * @param port The analog port that the distance sensor is plugged into
-     * @param lookupTable A 2D double array, first value is the distance, second value is the analog value 
-     */
-    public DistanceSensor(int port, double[][] lookupTable) {
-        sensor = new AnalogInput(port);
-
-        table = lookupTable;
-
-        useLookup = true;
-
-        sensor.setAccumulatorCenter(1401);
-        sensor.setAccumulatorDeadband(1056);
-        sensor.setAverageBits(8); // Sets how many readings should be averaged, 2^bits
-    }
 
     /**
      * Class for instantiating a distance sensor using an exponential fit equation
@@ -59,12 +34,7 @@ public class DistanceSensor {
         a = array.getDouble(0);
         b = array.getDouble(1);
         c = array.getDouble(2);
-        /* d = array.getDouble(3);
-        e = array.getDouble(4);
-        f = array.getDouble(5); */
         System.out.println(a);
-        
-        useLookup = false;
         
         sensor.setAverageBits(10); // Sets how many readings should be averaged
     }
@@ -119,35 +89,7 @@ public class DistanceSensor {
             return -2;
         }
 
-        if (!useLookup) {
-            //return ((a * Math.pow(x, b)) + c) / ((d * Math.pow(x, e)) + f); // Curve fit equation:
-                                                                                  // y\sim\frac{ax^b+c}{dx^f+g}
-                                                                                  // (Desmos)
-            return ((a * Math.pow(x, b)) + c); // Curve fit equation: y\sim ax^b+c
-        } else {
-            return lookupTable(sensor.getAverageVoltage());
-        }
-    }
-
-    private double lookupTable(double input) { // Uses a lookup table to find distance
-        double[] pdata = new double[2];
-        double m = -1; // Slope
-        double distance = -1;
-
-        if (input < 0.42) {
-            return -2;
-        }
-
-        for (double[] data : table) {
-            if (input < data[0]) {
-                m = (data[1] - pdata[1]) / (data[0] - pdata[0]);
-                distance = (m * (input - data[0])) + data[1];
-                return distance;
-            }
-            pdata[0] = data[0];
-            pdata[1] = data[1];
-        }
-        return -1;
+        return ((a * Math.pow(x, b)) + c); // Curve fit equation: y\sim ax^b+c
     }
 
 }
--- src/main/java/frc/team3373/robot/Robot.java
+++ src/main/java/frc/team3373/robot/Robot.java
@@ -7,19 +7,9 @@
 
 package frc.team3373.robot;
 
-import frc.team3373.autonomous.HABPlatformAuto;
-import frc.team3373.autonomous.Lineup;
-import frc.team3373.autonomous.VisionLineup;
-import frc.team3373.robot.SwerveControl.DriveMode;
-import frc.team3373.robot.SwerveControl.Side;
-
 import java.io.IOException;
 
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.RobotState;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 //import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -38,76 +28,18 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  public enum ObjectType {
-    HATCH, CARGO
-  }
-
-  int FRdriveMotorID = 2;
-	int FRrotateMotorID = 1;
-	int FREncHome = 358; // Zero values (value when wheel is turned to default					// zero- bolt hole facing front.)
-	int FREncMin = 10;
-	int FREncMax = 897;
-	
-	int BLdriveMotorID = 4;
-	int BLrotateMotorID = 3;
-	int BLEncHome = 265;
-	int BLEncMin = 11;
-	int BLEncMax = 902;
-	
-	int FLdriveMotorID = 8;
-	int FLrotateMotorID = 7;
-	int FLEncHome = 426;
-	int FLEncMin = 10;
-	int FLEncMax = 898;
-	
-	int BRdriveMotorID = 6;
-	int BRrotateMotorID = 5;
-	int BREncHome = 99;
-	int BREncMin = 10;
-	int BREncMax = 899;
-	
-	double robotWidth = 22.25; // TODO change robot dimensions to match this years robot
-  double robotLength = 16.25;
-  
-  SwerveControl swerve;
-
   SuperJoystick driver;
   SuperJoystick shooter;
-	
-  SuperAHRS ahrs;
-
-  DigitalInput line;
 
   DistanceSensor distl;
   DistanceSensor distr;
 
-  ObjectType object;
-
-  Claw claw;
-
-  Elevator elevator;
-
-  Vision vis;
-
-  HABPlatformAuto HABauto;
-
-  Compressor compressor;
-  //AutonomousControl control;
-  Solenoid armRelease;
-
-  Lineup lineup;
-  VisionLineup vlineup;
-
-  double rotateSpeedMod = .5;
-
-  boolean lockStraight = false;
-
-  boolean liftDirection = true;//1 is down, 0 is up
-  boolean startedlift = false;
-
-  //long startTime=0;
-
   int calInches = 3;
+
+  public enum ObjectType {
+    CARGO, HATCH
+  }
+  
   //private boolean cargoOpen;
   /**
    * This function is run when the robot is first started up and should be
@@ -119,50 +51,17 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-
-    distl = new DistanceSensor(2, 9); // !Temp
-
     try {
       Constants.loadConstants();
     } catch (IOException e) {
-      e.printStackTrace();
-      System.err.println("Catastrophic error tyring to load!");
+      try {
+        Constants.loadDefaults();
+      } catch (IOException f) {
+        f.printStackTrace();
+      }
     }
 
-    driver = new SuperJoystick(0);
-    shooter = new SuperJoystick(1);
-    ahrs = new SuperAHRS(SPI.Port.kMXP);
-    
-    compressor = new Compressor(1);
-    compressor.setClosedLoopControl(true);
-    compressor.start();
-
-    vis = new Vision();
-
-    swerve = new SwerveControl(FLrotateMotorID, FLdriveMotorID, FLEncMin, FLEncMax, FLEncHome, BLrotateMotorID,
-        BLdriveMotorID, BLEncMin, BLEncMax, BLEncHome, FRrotateMotorID, FRdriveMotorID, FREncMin, FREncMax, FREncHome,
-        BRrotateMotorID, BRdriveMotorID, BREncMin, BREncMax, BREncHome, ahrs, robotWidth, robotLength);
-
-    //joy1,joy2,swerve,relayid,PCMid,rightSolenoidFowardChannel,rightSolenoidReverseChannel,leftSolenoidFowardChannel,leftSolenoidReverseChannel,rightLimitSwitch,leftLimitSwitch,rightDistanceSensor,leftDistanceSensor
-    HABauto = new HABPlatformAuto(driver, shooter, swerve, ahrs, 0, 1, 1, 2, 0, 3, 1, 0, 2, 3);
-    claw = new Claw(2, 0, 3, 2, 1);
-    claw.open();
-    
-    distl = new DistanceSensor(0, 5);
-    distr = new DistanceSensor(1, 6);
-    line = new DigitalInput(2);
-
-    //control = new AutonomousControl(ahrs, swerve, distl, distr, driver, shooter, line);
-    elevator = new Elevator(5,4);
-    lineup = new Lineup(distl, distr, driver, swerve, line);
-    vlineup = new VisionLineup(distl, distr, driver, swerve, vis);
-
-    object = ObjectType.HATCH;
-
-    armRelease = new Solenoid(1, 7);
-    armRelease.set(true);
-    SmartDashboard.putString("Object", "HATCH");
-    SmartDashboard.putBoolean("Restore Defaults", false);
+    distl = new DistanceSensor(0, 9);
   }
 
   /**
@@ -228,14 +127,6 @@ public class Robot extends TimedRobot {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
-
-    SmartDashboard.setDefaultBoolean("Update Constants", false);
-    //elevator.resetCal();
-    //elevator.initPID();
-    // elevator.zero();
-
-    lockStraight = false;
-    compressor.setClosedLoopControl(true);
   }
 
   /**
@@ -252,23 +143,11 @@ public class Robot extends TimedRobot {
         // Put default auto code here
         break;
     }
-    driverControls();
-    elevator.refresh();
-    SmartDashboard.putNumber("Rotations", elevator.getRotations());
-    SmartDashboard.putNumber("Position", elevator.getPosition());
-    SmartDashboard.putNumber("FrontLeftDistance", distl.getDistance());
-    SmartDashboard.putNumber("FrontRightDistance", distr.getDistance());
   }
 
   @Override
   public void teleopInit() {
-    // SmartDashboard.setDefaultBoolean("Update Constants", false);
-    elevator.resetCal();
-    compressor.setClosedLoopControl(true);
-    // elevator.initPID();
-    // elevator.zero();
 
-    lockStraight = false;
   }
 
   /**
@@ -276,17 +155,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    //HABauto.update();
-    elevator.refresh();
-    driverControls();
-    SmartDashboard.putNumber("Rotations", elevator.getRotations());
-    SmartDashboard.putNumber("Position", elevator.getPosition());
-    SmartDashboard.putNumber("FrontLeftDistance", distl.getDistance());
-    SmartDashboard.putNumber("FrontRightDistance", distr.getDistance());
-    SmartDashboard.putBoolean("Lifting Up", liftDirection);
-    SmartDashboard.putNumber("Smart Avg", distl.getSmartAverage());
-    SmartDashboard.putNumber("Avg", distl.getAverage());
-    SmartDashboard.putNumber("Raw", distl.getVoltage());
+
   }
 
   /**
@@ -294,19 +163,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testInit() {
-    SmartDashboard.setDefaultBoolean("Update Constants", false);
-    SmartDashboard.setDefaultBoolean("Save Constants", false);
-    SmartDashboard.setDefaultBoolean("Restore Backup", false);
-    SmartDashboard.setDefaultNumber("Calibration Length", 1);
-    SmartDashboard.setDefaultBoolean("Restore Defaults", false);
-    SmartDashboard.putNumber("Inches", 10);
-    elevator.resetCal();
-    elevator.initPID();
-    compressor.setClosedLoopControl(true);
-    calInches = 3;
-    swerve.setControlMode(DriveMode.ROBOTCENTRIC);
-    swerve.printPositions();
-
+    
   }
 
   /**
@@ -314,274 +171,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
-    SmartDashboard.putNumber("Rotations", elevator.getRotations());
-    SmartDashboard.putNumber("Position", elevator.getPosition());
-    SmartDashboard.putNumber("FrontLeftDistance", distl.getDistance());
-    SmartDashboard.putNumber("FrontRightDistance", distr.getDistance());
-    //swerve.printPositions();
     
-    testControls();
-    elevator.refresh();
-    elevator.updatePID();
-    HABauto.update();
-    //VisionObject obj = vis.getObjectClosestToCenter();
-    //if(obj!=null){
-    //  obj.print();
-    //} else {
-    //  System.out.println("None");
-    //}
-  
-  }
-
-  public void driverControls() {
-    //################################################
-    //####          shared Controls               ####
-    //################################################
-    if (driver.isStartHeld() && shooter.isStartHeld()) {
-      HABauto.climb(25.5);
-      //auto get on HAB platform
-    } else if (driver.isBackHeld() && shooter.isBackHeld()) {
-      HABauto.climb(11);
-      //auto get on HAB platform
-    }
-    
-    //################################################
-    //####          Driver Controls               ####
-    //################################################
-    if (driver.isXPushed()) {
-      vlineup.cancel();
-      lockStraight = false;
-    }
-    
-    if (driver.isAPushed()) {
-      vlineup.align();
-      lockStraight = true;
-      //lockStraight = lineup.align(Lineup.AlignDirection.LEFT);
-    } else if (driver.isBPushed()) {
-      lockStraight = true;
-      //lockStraight = lineup.align(Lineup.AlignDirection.RIGHT);
-    }
-    
-    if (vlineup.isFinished()) {
-      if (driver.getRawAxis(2) > .5) {//FieldCentric
-        swerve.setControlMode(SwerveControl.DriveMode.FIELDCENTRIC);
-      } else if (driver.getRawAxis(3) > .5) {//RobotCentric
-        swerve.setControlMode(SwerveControl.DriveMode.ROBOTCENTRIC);
-      }
-      
-      if (driver.isLBHeld()) {//sniper
-        swerve.setDriveSpeed(0.1);
-        rotateSpeedMod = 1;
-      } else if (driver.isRBHeld() && elevator.getPosition() < 21) {//turbo
-        swerve.setDriveSpeed(0.5);
-        rotateSpeedMod = .7;
-      } else {//regular
-        if (elevator.getPosition() < 40) 
-          swerve.setDriveSpeed(0.4);
-        else {
-          swerve.setDriveSpeed(0.2);
-        }
-        rotateSpeedMod = .7;
-      }
-      
-      /*if(driver.isStartHeld()){
-        if(liftDirection){
-          HABauto.liftFront();
-        }else{
-          HABauto.lowerFront();
-        }
-        startedlift=true;
-      }else{
-        HABauto.stopFront();
-      }
-      
-      if(driver.isBackHeld()){
-        if(liftDirection){
-          HABauto.liftBack();
-        }else{
-          HABauto.lowerBack();
-        }
-        startedlift=true;
-      }else{
-        HABauto.stopBack();
-      }*/
-      
-      if (lockStraight) {
-        swerve.changeFront(Side.NORTH);
-        swerve.calculateAutoSwerveControl(180 - 90 * Math.ceil(-driver.getRawAxis(1)),
-            Math.max(Math.abs(driver.getRawAxis(1)), 0.05), 0);
-        if (driver.getRawAxis(1) > .5) lockStraight = false;
-      } else {
-        swerve.calculateSwerveControl(driver.getRawAxis(0), driver.getRawAxis(1),
-            driver.getRawAxis(4) * rotateSpeedMod);
-        /*if(driver.getRawAxis(0)>0.05 || driver.getRawAxis(1)>0.05){
-          if(startedlift){
-            HABauto.drive(true);
-          }else{
-            HABauto.drive(false);
-          }
-        }else{
-          HABauto.drive(false);
-        }*/
-        
-      }
-      
-      switch (driver.getPOV()) {
-      case 0:
-        swerve.changeFront(Side.WEST);
-        break;
-      case 90:
-        swerve.changeFront(Side.NORTH);
-        break;
-      case 180:
-        swerve.changeFront(Side.EAST);
-        break;
-      case 270:
-        swerve.changeFront(Side.SOUTH);
-        break;
-      }
-      
-      if (driver.isYPushed()) 
-        swerve.resetOrentation();
-    }
-    //swerve.controlMode(SwerveControl.DriveMode.FieldCentric);
-    
-    //################################################
-    //####           Shooter Controls             ####
-    //################################################
-    
-    if (shooter.isYPushed() && object == ObjectType.HATCH) {
-      claw.close();
-    } else if (shooter.isAPushed() && object == ObjectType.HATCH) {
-      claw.open();
-    } else if ((!shooter.isAHeld() && !shooter.isYHeld()) && object == ObjectType.CARGO) {
-      claw.close();
-    } else if ((shooter.isAHeld() || shooter.isYHeld()) && object == ObjectType.CARGO) {
-      claw.open();
-    }
-    
-    if (shooter.isLBPushed()) {
-      object = ObjectType.HATCH;
-      SmartDashboard.putString("Object", "HATCH");
-      claw.release(object);
-    } else if (shooter.isRBPushed()) {
-      object = ObjectType.CARGO;
-      SmartDashboard.putString("Object", "CARGO");
-      claw.grab(object);
-    }
-    
-    if (shooter.isXHeld()) {
-      elevator.stop();
-    }
-    
-    if (shooter.getRawAxis(1) < -0.5) {
-      claw.raise();
-    } else if (shooter.getRawAxis(1) > 0.5) {
-      claw.lower();
-    }
-    
-    if (shooter.isDPadDownPushed()) {
-      elevator.moveToPosition(0, object);
-    } else if (shooter.isDPadDownLeftPushed()) {
-      elevator.moveToPosition(1, object);
-    } else if (shooter.isDPadLeftPushed() || shooter.isDPadRightPushed()) {
-      elevator.moveToPosition(2, object);
-    } else if (shooter.isDPadUpPushed()) {
-      elevator.moveToPosition(3, object);
-    } else if (shooter.isBPushed()) {
-      elevator.moveToHeight(Constants.getNumber("elevatorMinHeight", 19));
-      //startTime = System.currentTimeMillis();
-    }
-    
-    /*if (shooter.isBHeld()) {
-      if (System.currentTimeMillis() - startTime > 1000 && startTime!=-2) {
-        elevator.zero();
-        startTime = -2;
-      }
-    }*/
-    
-    if (Math.abs(shooter.getRawAxis(5)) > 0.05) {
-      elevator.rawMovePID(-shooter.getRawAxis(5), 0.25);
-    }
-    
-    driver.clearButtons();
-    driver.clearDPad();
-    shooter.clearButtons();
-    shooter.clearDPad();
-  }
-
-  //################################################
-  //####         Calibration Controls           ####
-  //################################################
-  private void testControls() {
-    if (RobotState.isTest() && Math.abs(shooter.getRawAxis(5)) > 0.05) {
-      elevator.rawMovePID(-shooter.getRawAxis(5),0.25);
-    }
-
-    //swerve.printPositions();
-
-    if (shooter.isStartPushed()) {
-      elevator.calibrate();
-    } else if (shooter.isXPushed()) {
-      elevator.resetCal();
-    }
-
-    if (shooter.isBPushed()) {
-      elevator.moveToHeight(Constants.getNumber("elevatorHeight"));
-    }
-
-    if (shooter.isYPushed()) {
-      elevator.absoluteZero();
-    }
-
-    if (shooter.isAPushed()) {
-      System.out.println(calInches+" "+distl.getAverage());
-      calInches++;
-    }
-
-    if (driver.isAPushed()) {
-      vlineup.align();
-      lockStraight = true;
-      //control.lineup(Lineup.AlignDirection.LEFT);
-    }
-
-    if (driver.isXPushed()) {
-      vlineup.cancel();
-      lockStraight = false;
-    }
-
-    if (lockStraight) {
-      
-    } else {
-      if (driver.getRawAxis(2) > .5) {//FieldCentric
-        swerve.setControlMode(SwerveControl.DriveMode.FIELDCENTRIC);
-      } else if (driver.getRawAxis(3) > .5) {//RobotCentric
-        swerve.setControlMode(SwerveControl.DriveMode.ROBOTCENTRIC);
-      }
-
-      //swerve.calculateSwerveControl(driver.getRawAxis(0), driver.getRawAxis(1),
-      //      driver.getRawAxis(4) * rotateSpeedMod);
-    }
-
-    /* if(driver.isLBHeld()){
-      HABauto.liftBack();
-      HABauto.liftFront();
-    }else if(driver.isRBHeld()){
-      HABauto.lowerBack();
-      HABauto.lowerFront();
-    }else{
-      HABauto.stopBack();
-      HABauto.stopFront();
-    } */
-
-    if (driver.isBackHeld()) {
-      //HABauto.climb(28);
-      HABauto.climb(14);
-    }
-
-    shooter.clearButtons();
-    shooter.clearDPad();
-    driver.clearButtons();
-    driver.clearDPad();
   }
 }
